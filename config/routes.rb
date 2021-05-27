c601aaec60e378f1471e165fc82fabc63124be6f
Allison <allimacq@gmail.com>
2021-05-27 16:54:56 -0400

    revise/update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
   root to: "home#index"
-  resources :trails
-  scope "/admin" do
-    resources :users
+  resources :parks do
+    resources :activities
   end
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
