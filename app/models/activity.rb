f304f3281db9a059c0b447bad6158ef3ed9f9c30
Allison <allimacq@gmail.com>
2021-05-31 12:11:48 -0400

    add activerecord scope methods to activity and review model

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,4 +3,6 @@ class Activity < ApplicationRecord
     has_many :reviews
 
     validates_presence_of :name, :accessible?, :description
+
+    scope :accessible?, { where(accessible?: true) }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,6 @@ class Review < ApplicationRecord
 
     validates_presence_of :review, :rating
     validates :rating, length: { in: 1..5 }, numericality: { only_integer: true }
+
+    scope :rating, -> (amount) { where ("star > ?", amount) }
 end
