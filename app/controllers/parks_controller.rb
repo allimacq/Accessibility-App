a8d1bddd6a3a2a11ea4c90cbbc854230c852e0ac
Allison <allimacq@gmail.com>
2021-06-03 14:40:35 -0400

    add parks#new and start view. also add error partial for park

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,2 +1,24 @@
 class ParksController < ApplicationController
+
+    def new
+        @park = Park.new
+    end
+
+    def create
+        @park = Park.create(parks_params)
+        if @park.save
+            redirect_to park_path(@park)
+        else
+            render :new
+        end
+    end
+
+
+    private
+
+    def parks_params
+        params.require(:park).permit(:name, :state_id, :city_id)
+    end
+
+
 end
