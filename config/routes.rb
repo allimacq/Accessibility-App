0fc441e4b598897957b48b4943c1f8c1e59604db
Allison <allimacq@gmail.com>
2021-06-03 13:10:28 -0400

    add parks#show and route and view. create links to parks index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  get 'states/index'
+  resources :states, only: [:index, :show]
   root to: "states#index"
   resources :parks do
     resources :activities
