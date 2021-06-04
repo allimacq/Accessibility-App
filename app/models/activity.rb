4839d45ead41e193032d0ea6528cd962ab357dba
Allison <allimacq@gmail.com>
2021-06-04 12:49:12 -0400

    update usser, park, activity models to reflect user_id addition

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,6 +1,7 @@
 class Activity < ApplicationRecord
     has_many :parks
     has_many :reviews
+    belongs_to :user
 
     validates_presence_of :name, :accessible?, :description
 
--- app/models/park.rb
+++ app/models/park.rb
@@ -3,6 +3,7 @@ class Park < ApplicationRecord
     has_many :reviews, through: :activities
     belongs_to :city
     belongs_to :state
+    belongs_to :user
 
     validates :name, presence: true, uniqueness: true
 end
