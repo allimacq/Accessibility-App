bc7bf6f5ac7bc1478bf60f98be5b4263766e4870
Allison <allimacq@gmail.com>
2021-06-07 12:45:23 -0400

    add activities view folder and add strong params to activities controller

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -3,4 +3,11 @@ class ActivitiesController < ApplicationController
     def new
         @activity = Activity.new
     end
+
+    private
+
+    def parks_params
+        params.require(:park).permit(:name, :state_id, :city_id, :user_id, :accessible?)
+    end
+    
 end
