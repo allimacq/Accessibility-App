7bbb3033b94733008df386a22776d5a7168bbeef
Allison <allimacq@gmail.com>
2021-06-09 12:16:31 -0400

    edit activity model validations

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,7 +3,7 @@ class Activity < ApplicationRecord
     has_many :parks, through: :reviews
     belongs_to :user
 
-    validates_presence_of :name, :accessible, :description
+    validates_presence_of :name
 
     #scope :accessible, { where(accessible: true) }
 end
