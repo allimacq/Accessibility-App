1d8030ddf03699a327fb094e38506f8ba3e7bc9c
Allison <allimacq@gmail.com>
2021-06-14 23:26:40 -0400

    fix credentials for google oauth2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
     protected
 
     def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up)
-        devise_parameter_sanitizer.permit(:account_update)
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :password])
     end
 end
